507bc9d092b2f2fbf38bc8ecaf7312aa6bce4b75
cody.a.vanzandt@gmail.com <cav3@rice.edu>
2023-02-17 13:10:32 -0600

    Updating .gitignore to stop tracking .Rproj files; adding useful_utils.R

---
+++ useful_utils.R
@@ -0,0 +1,14 @@
+library(data.table)
+
+get_checkouts <- function(years, directory="./data/checkouts/"){
+  #' Returns a single dataframe of the Seattle checkouts data for the given years
+  #' 
+  #' @years - a vector of integer years
+  #' @direrctory - the path to the directory of data files.
+  #'               By default assumed to be ./data/checkouts/
+  #' 
+  #' Example: checkouts_2005_2006 <- get_checkouts(2005:2006)
+  selected_files <- file.path(directory, paste0("checkouts_", years, ".csv"))
+  individual_dataframes <- lapply(selected_files, fread)
+  rbindlist(individual_dataframes)
+}
