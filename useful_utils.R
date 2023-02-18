cee759d1fbf0cae3694aad4650b0c6b9ec0b1428
cody.a.vanzandt@gmail.com <cav3@rice.edu>
2023-02-17 18:05:15 -0600

    Forcing useful_utils/get_checkouts to return a dataframe

--- useful_utils.R
+++ useful_utils.R
@@ -12,5 +12,5 @@ get_checkouts <- function(years, directory="./data/checkouts/"){
   #' Example: checkouts_2005_2006 <- get_checkouts(2005:2006)
   selected_files <- file.path(directory, paste0("checkouts_", years, ".csv"))
   individual_dataframes <- lapply(selected_files, fread)
-  rbindlist(individual_dataframes)
+  as.data.frame(rbindlist(individual_dataframes))
 }
